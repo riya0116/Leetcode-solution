0b349b62314d4d631fe44916cf32c7aa450c67fb
riyasinha <missriya86446@gmail.com>
2026-04-16 22:56:57 +0530

    Add getConcatenation method for array duplication
    Implement a method to concatenate an array with itself.

---
+++ Arrays/array-getconcatenation.java
@@ -0,0 +1,16 @@
+// Problem: Concatenation of Array
+// Topic: Arrays
+// Pattern: Array Construction / Simulation
+// Time: O(n)
+// Space: O(n)
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] result = new int[2*nums.length];
+        for(int i=0;i<nums.length;i++){
+            result[i] = nums[i];
+            result[i+nums.length] = nums[i];
+        }
+        return result;
+    }
+}
